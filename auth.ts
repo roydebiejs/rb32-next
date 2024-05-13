c7c083b55fa87a663f4e5d538a6453e98fd8b3ff
roy-stapp1solutions <roy@stapp1.solutions>
2024-05-13 21:42:12 +0200

    Progress

--- auth.ts
+++ auth.ts
@@ -1,10 +1,42 @@
-import NextAuth from "next-auth";
+import NextAuth, { type DefaultSession } from "next-auth";
+
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "@/auth.config";
+import { getUserById } from "@/data/user";
+import { UserRole } from "@prisma/client";
 
 import { db } from "@/lib/db";
 
+declare module "next-auth" {
+  interface Session {
+    user: {
+      role: "ADMIN" | "USER";
+    } & DefaultSession["user"];
+  }
+}
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      if (session.user && token.role) {
+        session.user.role = token.role as UserRole;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
